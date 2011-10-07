1ffd1dfe5ca2e153fbb1e424bbf8c67efb0b7fd1
Thijs Metsch <tmetsch@openindy.(none)>
2011-10-07 12:24:13 +0200

    code cleanup

--- dtrace.py
+++ dtrace.py
@@ -1,101 +1,138 @@
-from ctypes import *
+#!/usr/bin/env python2.7
+
+'''
+Simple Python based DTrace consumers which executes a Hello World D script
+using libdtrace and ctypes to access it.
+'''
+from ctypes import cdll, CDLL, Structure, c_void_p, c_char_p, c_uint, c_int, \
+c_char, CFUNCTYPE, POINTER, byref
+
+
+SCRIPT = 'dtrace:::BEGIN {trace("Hello World");}'
+
 
 class dtrace_bufdata(Structure):
+    '''
+    As defined in dtrace.h:310
+    '''
     _fields_ = [("dtbda_handle", c_void_p),
-                ("dtbda_buffered", c_char_p),
+                ("dtbda_buffered", c_char_p),       # works
                 ("dtbda_probe", c_void_p),
                 ("dtbda_recdesc", c_void_p),
-                ("dtbda_aggdata",c_void_p),
+                ("dtbda_aggdata", c_void_p),
                 ("dtbda_flags", c_uint)
         ]
 
+
 class dtrace_probedesc(Structure):
+    '''
+    As defined in sys/dtrace.h:884
+    '''
     _fields_ = [("dtrace_id_t", c_uint),
                 ("dtpd_provider", c_char),
                 ("dtpd_mod", c_char),
-                ("dtpd_func", c_char),
-                ("dtpd_name", c_char * 64)]
+                ("dtpd_func", c_char_p),
+                ("dtpd_name", c_char_p)]
+
 
 class dtrace_probedata(Structure):
+    '''
+    As defined in dtrace.h:186
+    '''
     _fields_ = [("dtpda_handle", c_void_p),
                ("dtpda_edesc", c_void_p),
                ("dtpda_pdesc", dtrace_probedesc),
-               ("dtpda_cpu", c_int),
+               ("dtpda_cpu", c_int),                # works
                ("dtpda_data", c_void_p),
                ("dtpda_flow", c_void_p),
                ("dtpda_prefix", c_void_p),
                ("dtpda_indent", c_int)]
-    pass
+
 
 class dtrace_recdesc(Structure):
+    '''
+    As defined in sys/dtrace.h:931
+    '''
     pass
 
 
-CHEW_FUNC = CFUNCTYPE(c_int, POINTER(dtrace_probedata), POINTER(c_void_p))
+CHEW_FUNC = CFUNCTYPE(c_int,
+                      POINTER(dtrace_probedata),
+                      POINTER(c_void_p))
 CHEWREC_FUNC = CFUNCTYPE(c_int,
                          POINTER(dtrace_probedata),
                          POINTER(dtrace_recdesc),
                          POINTER(c_void_p))
-BUFFERED_FUNC = CFUNCTYPE(c_int, POINTER(dtrace_bufdata), POINTER(c_void_p)) 
+BUFFERED_FUNC = CFUNCTYPE(c_int,
+                          POINTER(dtrace_bufdata),
+                          POINTER(c_void_p))
+
+cdll.LoadLibrary("libdtrace.so")
+
+LIBRARY = CDLL("libdtrace.so")
+
 
 def chew_func(data, arg):
-##    print dir(data.contents)
+    '''
+    Callback for chew.
+    '''
     print 'cpu :', c_int(data.contents.dtpda_cpu).value
-
-##    print dir(data.contents.dtpda_pdesc)
-##    print 'probe id :', c_uint(data.contents.dtpda_pdesc.dtrace_id_t).value
-    
-    #print len(data.contents.dtpda_pdesc.dtpd_name)
-    #print 'probe name :', c_char(data.contents.dtpda_pdesc.dtpd_name[0])
-    
     return 0
 
+
 def chewrec_func(data, rec, arg):
+    '''
+    Callback for record chewing.
+    '''
     if rec == None:
         return 1
     return 0
 
+
 def buffered(bufdata, arg):
+    '''
+    In case dtrace_work is given None as filename - this one is called.
+    '''
     print c_char_p(bufdata.contents.dtbda_buffered).value.strip()
     return 0
 
-if __name__ == '__main__':
-    cdll.LoadLibrary("libdtrace.so")
 
-    LIBRARY = CDLL("libdtrace.so")
-
-    d = 'dtrace:::BEGIN {trace("Hello World");}'
-
-    # 1. get dtrace handle
+def run_dtrace():
+    '''
+    Go for it...
+    '''
+    # get dtrace handle
     handle = LIBRARY.dtrace_open(3, 0, byref(c_int(0)))
 
-    # 2. options
+    # options
     if LIBRARY.dtrace_setopt(handle, "bufsize", "4m") != 0:
         txt = LIBRARY.dtrace_errmsg(handle, LIBRARY.dtrace_errno(handle))
         raise Exception(c_char_p(txt).value)
 
-    # 3. callbacks
+    # callbacks
     buf_func = BUFFERED_FUNC(buffered)
     LIBRARY.dtrace_handle_buffered(handle, buf_func, None)
 
-    # 4. compile
-    prg = LIBRARY.dtrace_program_strcompile (handle, d, 3, 4, 0, None)
+    # compile
+    prg = LIBRARY.dtrace_program_strcompile(handle, SCRIPT, 3, 4, 0, None)
 
-    # 5. run
+    # run
     LIBRARY.dtrace_program_exec(handle, prg, None)
     LIBRARY.dtrace_go(handle)
     LIBRARY.dtrace_stop(handle)
 
-    # do work and output to file
+    # do work
     LIBRARY.dtrace_sleep(handle)
     chew = CHEW_FUNC(chew_func)
     chew_rec = CHEWREC_FUNC(chewrec_func)
-    LIBRARY.dtrace_work(handle, None, chew, chew_rec, None);
+    LIBRARY.dtrace_work(handle, None, chew, chew_rec, None)
 
-    #LIBRARY.dtrace_aggregate_print(handle, "out.txt", None);
-
+    # Get errors if any...
     txt = LIBRARY.dtrace_errmsg(handle, LIBRARY.dtrace_errno(handle))
     print c_char_p(txt).value
 
     # Last: close handle!
-    LIBRARY.dtrace_close(handle);
+    LIBRARY.dtrace_close(handle)
+
+if __name__ == '__main__':
+    run_dtrace()
