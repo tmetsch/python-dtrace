c0104447e78cab597981f9bb3294ee7b4d91b703
Thijs Metsch <tmetsch@gmail.com>
2020-12-30 21:36:35 +0100

    code cleanup.

--- setup.py
+++ setup.py
@@ -22,17 +22,23 @@ try:
         if not os.path.exists(os.path.join(src_dir, "sys/cddl")):
             raise ImportError("Cannot find FreeBSD DTrace headers")
         extra_args["include_dirs"] = [
-            os.path.join(src_dir, "sys/cddl/compat/opensolaris"),
-            os.path.join(src_dir, "sys/cddl/contrib/opensolaris/uts/common"),
-            os.path.join(src_dir, "cddl/contrib/opensolaris/lib/libdtrace/common"),
+            os.path.join(src_dir,
+                         "sys/cddl/compat/opensolaris"),
+            os.path.join(src_dir,
+                         "sys/cddl/contrib/opensolaris/uts/common"),
+            os.path.join(src_dir,
+                         "cddl/contrib/opensolaris/lib/libdtrace/common"),
         ]
     BUILD_EXTENSION = {'build_ext': build_ext}
-    EXT_MODULES = cythonize([
-        Extension("dtrace", ["dtrace_cython/dtrace_h.pxd",
-                             "dtrace_cython/consumer.pyx"],
-                  libraries=["dtrace"],
-                  **extra_args)],
-        language_level=sys.version_info.major)
+    EXT_MODULES = cythonize(
+        [
+            Extension("dtrace", ["dtrace_cython/dtrace_h.pxd",
+                                 "dtrace_cython/consumer.pyx"],
+                      libraries=["dtrace"],
+                      **extra_args)
+        ],
+        language_level=sys.version_info.major
+    )
 
 except ImportError:
     BUILD_EXTENSION = {}
@@ -45,8 +51,8 @@ except ImportError:
 setup(name='python-dtrace',
       version='0.0.11',
       description='DTrace consumer for Python based on libdtrace. Use Python'
-                  + ' as DTrace Consumer and Provider! See the homepage for'
-                  + ' more information.',
+                  ' as DTrace Consumer and Provider! See the homepage for'
+                  ' more information.',
       license='MIT',
       keywords='DTrace',
       url='http://tmetsch.github.com/python-dtrace/',
