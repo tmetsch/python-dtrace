1cc6897eded5af05ec210d5fd08489d0ab5b9a26
Thijs Metsch <tmetsch@gmail.com>
2022-05-13 21:26:10 +0200

    new bugfix release

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ except ImportError:
 
 
 setup(name='python-dtrace',
-      version='0.0.14',
+      version='0.0.15',
       description='DTrace consumer for Python based on libdtrace. Use Python'
                   ' as DTrace Consumer and Provider! See the homepage for'
                   ' more information.',
